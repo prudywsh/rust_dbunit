80672ea667a047c5d175759f2dd98455797fde70
Antoine Prudhomme <antoineprudhomme5@gmail.com>
2020-11-27 23:13:19 +0100

    get create table query

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,23 @@ fn get_db_conn() -> mysql::PooledConn {
 }
 
 fn get_tables_names(conn: &mut mysql::PooledConn) -> Vec<String> {
-    conn.query_map("show tables;", |table_name| table_name)
+    conn.query_map("SHOW TABLES;", |table_name| table_name)
         .unwrap()
+}
+
+fn get_table_create_query(conn: &mut mysql::PooledConn, table_name: String) -> String {
+    let query = format!("SHOW CREATE TABLE {}", table_name);
+    let row = conn.query_first(query).unwrap();
+    let (_, create_table_query): (String, String) = row.unwrap();
+    create_table_query
 }
 
 fn main() {
     let mut conn = get_db_conn();
 
-    let tables_names = get_tables_names(&mut conn);
+    // let tables_names = get_tables_names(&mut conn);
+    // println!("{:?}", tables_names);
 
-    println!("{:?}", tables_names);
+    let create_table_query = get_table_create_query(&mut conn, String::from("companies"));
+    println!("{:}", create_table_query);
 }
